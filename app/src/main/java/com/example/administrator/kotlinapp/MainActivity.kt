bcd7a1d3c2dac4930874a3eee9f23882534bd525
QLY <1532888097@qq.com>
2018-01-02 17:46:44 +0800

    增加加载圈

--- app/src/main/java/com/example/administrator/kotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/administrator/kotlinapp/MainActivity.kt
@@ -9,6 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        circleview.setProgressNum(5000)
         circleview_100.setProgressNum(5000)
     }
 
